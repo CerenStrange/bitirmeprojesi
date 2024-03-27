e75828318d4e6df829289d05c97aa870175e4678
Ceren Çelik <157277848+CerenStrange@users.noreply.github.com>
2024-03-27 12:35:18 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
-
+import burfas from './assets/burfas.png'
 
 
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary" >
   <div  className="container">
-    <a  className="navbar-brand" href="#"><img src="src/assets/burfas.png" width="100px"/></a>
+    <a  className="navbar-brand" href="#"><img src={burfas} width="100px"/></a>
     <button  className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span  className="navbar-toggler-icon"></span>
     </button>
